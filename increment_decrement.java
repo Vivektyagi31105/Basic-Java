805cafb20d333e377d7a2fc7837dc5cf548841be
Vivek Tyagi <105948234+Vivektyagi31105@users.noreply.github.com>
2023-07-15 23:36:34 +0530

    Add files via upload

---
+++ increment_decrement.java
@@ -0,0 +1,35 @@
+import java.util.*;
+public class increment_decrement{
+    public static void main(String args[]){
+        // Scanner scn = new Scanner(System.in);
+        // System.out.println("Input number:");
+        // int num1 = scn.nextInt();
+        // System .out.println("Output is :"+num1);
+
+        // Increment questions
+        // int i=1;
+
+        // System.out.println(i++ + i++);
+        // System.out.println(++i);
+        // System.out.println(i++);
+
+        // Decrement questions
+
+        int j=1;
+
+        // System.out.println(j--);
+        // System.out.println(--j);
+
+     System.out.println(++j);
+     System.out.println(j--);
+     System.out.println(--j);
+     System.out.println(++j);
+     System.out.println(j--);
+     System.out.println(j--);
+     System.out.println(--j);
+      
+
+  
+    }
+}
+
