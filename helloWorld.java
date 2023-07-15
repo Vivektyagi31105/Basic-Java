5c436485883e578218a44a05cf4b9c0698b204d2
Vivek Tyagi <105948234+Vivektyagi31105@users.noreply.github.com>
2023-07-15 23:33:43 +0530

    Add files via upload

---
+++ helloWorld.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class helloWorld
+{
+    public static void main (String args[]){
+       System.out.println("Hello World"); 
+       System.out.print("Hello World");
+        System.out.println("Hello World");
+
+
+        
+
+    }
+
+}
