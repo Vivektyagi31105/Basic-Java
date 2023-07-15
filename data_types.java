3841e910c13036c4fc868e5d4b067c28c313684e
Vivek Tyagi <105948234+Vivektyagi31105@users.noreply.github.com>
2023-07-15 23:34:01 +0530

    Add files via upload

---
+++ data_types.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class data_types {
+    public static void main(String[] args) {
+        
+        int x= 10;
+        int y= 11;
+
+        System.out.println(x+","+y);
+        System.out.println(x);
+        System.out.println(y);
+
+        System.out.println(x+y);
+        int z= x+y;
+        System.out.println(z);
+
+        long big_Number=1223456789;
+        System.out.println(big_Number);
+
+        float d= 12.234f;
+
+        System.out.println(d);
+
+        double e = 234.4;
+        System.out.println(e);
+
+
+    }
+}
