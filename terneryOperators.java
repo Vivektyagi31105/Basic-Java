25dc429d8b5557219c17802129493913018a2c52
Vivek Tyagi <105948234+Vivektyagi31105@users.noreply.github.com>
2023-07-16 23:34:07 +0530

    Add files via upload

---
+++ terneryOperators.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class terneryOperators {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Enter marks");
+        int marks = scn.nextInt();
+       String result = (marks>33)? "Pass": "Fail";
+
+       System.out.println(result);
+
+       System.out.println("Enter number x:");
+       int x= scn.nextInt();
+       int y;
+
+       y= (x>=10)? 1: 0;
+       System.out.println(y);
+    }
+}
