1544399a8e56e4658afa3a2ba00f9ad9b26e1d53
Vivek Tyagi <105948234+Vivektyagi31105@users.noreply.github.com>
2023-07-16 23:25:45 +0530

    Add files via upload

---
+++ if_else_condition.java
@@ -0,0 +1,27 @@
+public class if_else_condition {
+    public static void main(String[] args) {
+        int a=234;
+        int b= 232438;
+
+        if(a>b){
+            System.out.println("A is greater than B");
+        }else{
+            System.out.println("B is greator than A");
+        }
+
+        int a1= 2345;
+        int a2=53453;
+        int a3= 2309432;
+
+        if(a1>a2){
+            System.out.println("a1 is greater than a2");
+        }else if(a2>a3){
+            System.out.println("a2 is greater than a3");
+        }else{
+            System.out.println("a3 is than a1");
+        }
+
+        
+
+    }
+}
