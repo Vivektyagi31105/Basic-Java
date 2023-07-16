048891d9c2205511e21d00074ff347f1573e4d6a
Vivek Tyagi <105948234+Vivektyagi31105@users.noreply.github.com>
2023-07-16 23:24:17 +0530

    Add files via upload

---
+++ assignmentOperators.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class assignmentOperators {
+
+    public static void main(String[] args) {
+        int i=10;
+
+        int j=0;
+        j=j+1;
+        j+=1;
+
+        System.out.println(j);
+        System.out.println(i+=5);
+        System.out.println(i-=3);
+        System.out.println(j*=2);
+        System.out.println(i/=2);
+    }
+}
