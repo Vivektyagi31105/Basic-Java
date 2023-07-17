3c7ee3f9e0554b260cf4a142d77853b1253c5839
Vivek Tyagi <105948234+Vivektyagi31105@users.noreply.github.com>
2023-07-17 22:48:01 +0530

    Add files via upload

---
+++ type_casting.java
@@ -0,0 +1,37 @@
+import java.util.*;
+/**
+ * type_casting
+ */
+public class type_casting {
+
+    public static void main(String[] args) {
+    int a= 10;
+    int b= 5;
+
+    double c= a/b;
+    System.out.println(c);
+
+    double a1= 10;
+    double a2= 5;
+    
+    int i= (int) a1;
+    int j= (int) a2;
+
+    int k= (int)a1/ (int)a2;
+    System.out.println(k);
+
+    int a3 =2;
+    int c3= (int) a1/a3;
+    System.out.println(c3);
+
+
+    System.out.println(i/j);
+
+    int A= 10;
+    int B= 3;
+
+    double C= A/B;
+
+    System.out.println(C);
+}
+}
