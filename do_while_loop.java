d9b5502ad80d9da880dc928497d71cd65f544c08
Vivek Tyagi <105948234+Vivektyagi31105@users.noreply.github.com>
2023-07-17 22:46:58 +0530

    Add files via upload

---
+++ do_while_loop.java
@@ -0,0 +1,10 @@
+public class do_while_loop {
+    public static void main(String[] args) {
+        int i=10;
+
+        do{
+          System.out.println("*");
+         i++;
+        }while(i<20);
+    }
+}
