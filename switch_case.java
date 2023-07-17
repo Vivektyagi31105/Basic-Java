f85303520c2dc9e28670d07c42fa5160adafa2f0
Vivek Tyagi <105948234+Vivektyagi31105@users.noreply.github.com>
2023-07-17 22:48:39 +0530

    Add files via upload

---
+++ switch_case.java
@@ -0,0 +1,24 @@
+public class switch_case {
+    public static void main(String[] args) {
+        int num= 500;
+        
+        switch(num){
+            case 200:
+            System.out.println("case 1");
+            break;
+
+            case 300:
+            System.out.println("case 2");
+              break;
+            case 100:
+
+            System.out.println("Case 3");
+            break;
+
+            default:
+            System.out.println("Not found");
+            
+        }
+
+    }
+}
