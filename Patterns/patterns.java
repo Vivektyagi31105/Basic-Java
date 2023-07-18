fb502ac40b48ac2f79cf82689a1ce32337281384
Vivek Tyagi <105948234+Vivektyagi31105@users.noreply.github.com>
2023-07-18 23:16:50 +0530

    Add files via upload

---
+++ Patterns/patterns.java
@@ -0,0 +1,17 @@
+package Patterns;
+
+import java.util.*;
+public class patterns {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n= scn.nextInt();
+
+        for(int i=1; i<=n;i++){
+            for(int j=1; j<=4; j++){
+                System.out.print("*"+" ");
+            }
+            System.out.println();
+        }
+    }
+    
+}
