8cba0cf4f346176ff5945c133ec135f3d0307338
Vivek Tyagi <105948234+Vivektyagi31105@users.noreply.github.com>
2023-07-22 23:02:07 +0530

    Update terneryOperators.java

--- terneryOperators.java
+++ terneryOperators.java
@@ -1,6 +1,7 @@
 import java.util.*;
 
 public class terneryOperators {
+    
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         System.out.println("Enter marks");
