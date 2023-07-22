8b63cda70671ff84f1a2687c35c1963eace2a374
Vivek Tyagi <105948234+Vivektyagi31105@users.noreply.github.com>
2023-07-22 22:59:26 +0530

    Update hello_World.java

--- hello_World.java
+++ hello_World.java
@@ -3,6 +3,7 @@ import java.util.*;
 public class hello_World
 {
     public static void main (String args[]){
+        
        System.out.println("Hello World"); 
        System.out.print("Hello World");
         System.out.println("Hello World");
