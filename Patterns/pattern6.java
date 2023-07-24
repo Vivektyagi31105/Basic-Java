4e1d410357ca9d21c75b88395261a692c73e9078
Vivek Tyagi <105948234+Vivektyagi31105@users.noreply.github.com>
2023-07-24 23:39:18 +0530

    Add files via upload

---
+++ Patterns/pattern6.java
@@ -0,0 +1,26 @@
+package Patterns;
+
+public class pattern6 {
+    public static void main(String[] args) {
+        int n=5;
+        int  space = 0;
+         int star = n;
+        for(int i=1; i<=n; i++)
+        {
+            for(int j=1; j<=star; j++)
+            
+            {
+                System.out.print("* ");
+
+            }
+            for(int j=1; j<=space; j++)
+            {
+                System.out.print(" ");
+            }
+            System.out.println();
+            star--;
+            space++;
+        }
+    }
+
+}
