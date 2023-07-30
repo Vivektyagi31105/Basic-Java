e30c50e9d871ca19b36b850cc9192344817c8465
Vivek Tyagi <105948234+Vivektyagi31105@users.noreply.github.com>
2023-07-30 23:23:55 +0530

    Update pattern_7.java

--- Patterns/pattern_7.java
+++ Patterns/pattern_7.java
@@ -1,6 +1,7 @@
 import java.util.*;
 
 public class pattern_7 {
+    
     public static void main(String[] args) {
            Scanner scn = new Scanner(System.in);
           int n = scn.nextInt();
