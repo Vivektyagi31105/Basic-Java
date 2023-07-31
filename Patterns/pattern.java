9a8e4d4204a8376f90c6d6370aacb3bbcd5d5652
Vivek Tyagi <105948234+Vivektyagi31105@users.noreply.github.com>
2023-07-31 23:25:40 +0530

    Update pattern.java

--- Patterns/pattern.java
+++ Patterns/pattern.java
@@ -1,6 +1,7 @@
 import java.util.*;
 
 public class rightAngle_pattern {
+    
     public static void main(String[] args) {
         Scanner scn = new Scanner (System.in);
         int n = scn.nextInt();
