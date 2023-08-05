64e3543195ba7d45866498af7f9e9025cbd8b487
Vivek Tyagi <105948234+Vivektyagi31105@users.noreply.github.com>
2023-08-05 23:34:25 +0530

    Update hello_World.java

--- hello_World.java
+++ hello_World.java
@@ -4,7 +4,9 @@ public class hello_World{
     public static void main (String args[]){
         
        System.out.println("Hello World"); 
+        
        System.out.print("Hello World");
+        \
         System.out.println("Hello World");
 
 
